afa42037961fca1c4a7904fc14ecbf50de343a42
root <root@DESKTOP-024B97B.localdomain>
2021-08-29 14:22:01 -0700

    making lint happy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,16 @@
+import React from 'react'
 import NoSSR from 'react-no-ssr'
 import 'react-simple-keyboard/build/css/index.css'
-
 // eslint-disable-next-line import/extensions
 import '_/components/TipTapStyles.scss'
 import {AppHandler} from '_/handlers/AppHandler'
 
-export default (props) => {
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+const App = (props) => {
   return (
     <NoSSR>
       <AppHandler {...props} />
     </NoSSR>
   )
 }
+export default App
